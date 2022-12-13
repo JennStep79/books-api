ea72873cecc7e4634e98d33b6e5e2072837e3fd4
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2022-12-13 14:17:55 -0800

    create routes for update, show, delete, and index with CORS enabled

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 // DEPENDENCIES
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
-// app.use(express.json())
 const app = express()
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) })
 
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
 
 // ROUTES
 app.get('/', (req, res) => {
