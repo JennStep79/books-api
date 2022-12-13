d9c46cb1f2a844ca505009c4ff718104cf7b0fb8
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2022-12-12 23:40:46 -0800

    setup exoress API and route with dummy text

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
+// DEPENDENCIES
 const express = require('express')
 const mongoose = require('mongoose')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+// app.use(express.json())
+const app = express()
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) })
+
+// MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const booksControllers = require('./controllers/books_controllers')
+app.use('/books', booksControllers)
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log('Greetings! From port: ', PORT);
+})
